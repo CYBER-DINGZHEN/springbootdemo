012d1d106d4edd8e7d43409b8d947537e43cb544
Panyuan <ipanyuan@outlook.com>
2024-01-17 22:44:54 +0800

    git test1

---
+++ git_test/src/main/java/com/itheima/Main.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击间距中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("git test");
+        }
+    }
+}
